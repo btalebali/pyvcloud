e90b1bd49f800aba0ae9a9998af03a571cc76ee1
Béchir TALEB ALI <btalebali@prologue.fr>
2015-06-24 16:53:35 +0200

    update requirement for python 2.7.9

--- VMwarevCloud/testvCloudDirector.py
+++ VMwarevCloud/testvCloudDirector.py
@@ -6,14 +6,16 @@ from time import sleep
 ############## Internat Prologue 's VMware vCloud Director
 vcloudconfig={'host': 'vcloudcell.prologue.prl', 'username': 'uicbm', 'password': 'u15i21cb21m0',
               'org': 'prologue', 'instance':'','vdc_name': 'vDC_prologue','service_type_name': 'vcd','VCD_version':'5.5','cert': '/root/git/pyvcloud/VMwarevCloud/certvcloud.cer','logFile':'pyvcd.log'}
-#cert=/root/git/pyvcloud/VMwarevCloud/vcloudcell.prologue.prl
 
-vcloudVM= {'vApptemplate': 'Ubuntu12.04 x86_64', 'cataloguen': 'Linux','vAppname': 'VM42','status':'','name':'','cpus':'','memory':'',
+
+
+vcloudVM= {'vApptemplate': 'Ubuntu12.04 x86_64', 'cataloguen': 'Linux','vAppname': 'VMkh','status':'','name':'','cpus':'','memory':'',
            'owner':'','os':'','rootpassword':'','computer_name':'uicb','privateaddress':'','publicaddress':'','macaddress':''
            ,'customization_script':'touch /tmp/file1219','access':'public','region':''}
 
-vCloudvDCnet={'GWname':'EdgeGW','vDCorg_net':'vDCORGnet0','start_address':'10.10.0.2','end_address':'10.10.0.50','gateway_ip':'10.10.0.1',
+vCloudvDCnet={'GWname':'EdgeGW','vDCorg_net':'vDCORGnetkh','start_address':'10.10.0.2','end_address':'10.10.0.254','gateway_ip':'10.10.0.1',
               'netmask':'255.255.255.0','dns1':'8.8.8.8','dns2':'8.8.4.4','dns_suffix':None,'FWrules':'' }
+
 
 '''
 ############## Alhambra'is VMware vCloud director
